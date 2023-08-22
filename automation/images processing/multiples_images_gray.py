4ffaacd3404dcfa0ec6d72563dfeec03cb411132
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-08-21 22:10:33 -0300

    melhorias estudos de AI

--- automation/images processing/multiples_images_gray.py
+++
@@ -1,8 +0,0 @@
-import os
-import cv2
-
-images = os.listdir("images")
-print(images)
-for image in images:
-    gray = cv2.imread(f"images/{image}",0)
-    cv2.imwrite(f"grayimages/gray-{image}",gray)
--- automation/sms/methods/format.py
+++
@@ -1,5 +0,0 @@
-
-def clean_text(text):
-    """Extract the temperature data from"""
-    output = float(text.split(": ")[1])
-    return output
